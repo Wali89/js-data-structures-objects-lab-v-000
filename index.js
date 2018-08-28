47fa905ed79a5dbef437beefe0a54d56b880d9d2
Wali Rahman <wali.rahman89@yahoo.com>
2018-08-28 10:22:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const driver = {};
 function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({}, driver, { [key]: value });
 }
+
+function desctructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
